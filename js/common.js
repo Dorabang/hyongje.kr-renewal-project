02a4132e65c08d2dd31b8251e8aeef35e44e8553
Dorabang <psa1195@naver.com>
2022-12-14 20:58:49 +0900

    mgnb_wrap add

--- js/common.js
+++ js/common.js
@@ -1,49 +1,65 @@
 $(document).ready(function () {
+    // aos
+    AOS.init();
 
+    // gotop
+    $(".gotop").hide();
 
-  // aos
-  AOS.init();
+    $(".gotop").click(function () {
+        $("html").animate(
+            {
+                scrollTop: 0,
+            },
+            500
+        );
+    });
 
+    $(window).scroll(function () {
+        if ($(this).scrollTop() > 500) {
+            $(".gotop").fadeIn();
+        } else {
+            $(".gotop").fadeOut();
+        }
+    });
 
-  // gotop
-  $('.gotop').hide();
-  
-  $('.gotop').click(function(){
-    $('html').animate({
-      scrollTop : 0
-    },500);
-  });
+    // header fix
+    $(window).scroll(function () {
+        if ($(this).scrollTop() > 500) {
+            $("#header").addClass("fix");
+        } else {
+            $("#header").removeClass("fix");
+        }
+    });
 
-  $(window).scroll(function(){
-    if ($(this).scrollTop() > 500) {
-      $('.gotop').fadeIn();
-    }
-    else {
-      $('.gotop').fadeOut();
-    }
-  });
+    // popup
+    $(".popup_close").click(function () {
+        $(".popup").slideUp();
+    });
 
+    // depth2
+    $(".depth2").hide();
+    $(".gnb > li").hover(function () {
+        $(this).find(".depth2").stop().fadeToggle();
+    });
 
-  // header fix
-  $(window).scroll(function () {
-    if ($(this).scrollTop() > 500) {
-      $('#header').addClass('fix');
-    } else {
-      $('#header').removeClass('fix');
-    }
-  });
+    // mdepth2
+    $(".mgnb_wrap .mgnb .mdepth2").hide();
 
+    $(".mgnb_wrap .mgnb > li").hover(function () {
+        $(this).find(".mdepth2").stop().slideToggle();
+    });
 
-  // popup
-  $('.popup_close').click(function () {
-    $('.popup').slideUp();
-  });
-  
+    // mgnb_wrap show
+    $(".ham").click(function () {
+        $(".mgnb_wrap").animate({
+            left: "10%",
+        });
+    });
 
-  // depth2
-  $('.depth2').hide();
-  $('.gnb > li').hover(function() {
-    $(this).find('.depth2').stop().fadeToggle();
-  });
-
+    // mgnb_wrap close
+    $(".mgnb_wrap .btn_close").click(function () {
+        $(".mgnb_wrap").animate({
+            left: "100%",
+        });
+    });
 });
